9bca2cad3696bc1c229bccb874944027667b391a
Pedro Henrique <phpessoal048@gmail.com>
2025-07-11 17:43:13 -0300

    feact/home-page/fuction js

--- static/js/index.js
+++ static/js/index.js
@@ -1,11 +1,46 @@
-function atualizarConteudo(){
+function partialHome(){
+    var body = document.querySelector("main .container-home")
+    body.innerHTML = ""
     var ell = document.createElement("p");
     var text = document.createTextNode("minha page home")
-    var body = document.querySelector("main div")
     ell.appendChild(text);
     body.appendChild(ell)
     console.log(body)
 }
 
-document.addEventListener("DOMContentLoaded", atualizarConteudo);
+function partialNewBlog(){
+    var body = document.querySelector("main .container-home")
+    body.innerHTML = ""
+    
+    // Form para o novo blog
+    var formNewBlog = document.createElement("form");
+    formNewBlog.id = "form-new-post"
+
+    var inputTitleLabel =document.createElement("label")
+    inputTitleLabel.textContent = "Titulo"
+    inputTitleLabel.classList.add("form-label")
+
+    var inputTitle = document.createElement("input")
+    inputTitle.type = "text"
+    inputTitle.classList.add("form-control")
+
+    var inputDescriptionLabel =document.createElement("label")
+    inputDescriptionLabel.textContent = "Descrição"
+    inputDescriptionLabel.classList.add("form-label")
+
+    var inputDescription =document.createElement("input")
+    inputDescription.type = "text-area"
+    inputDescription.classList.add("form-control") 
+
+    formNewBlog.appendChild(inputTitleLabel)
+    formNewBlog.appendChild(inputTitle)
+    formNewBlog.appendChild(inputDescriptionLabel)
+    formNewBlog.appendChild(inputDescription)
+
+    body.appendChild(formNewBlog)
+    console.log(body)
+}
+
+
+document.addEventListener("DOMContentLoaded", partialHome);
 
